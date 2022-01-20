080c733ce801789d87c42a57c68a374c62773b63
Victor Dziedzorm <dicksonsmart0711@gmail.com>
2022-01-20 15:46:04 +0000

    Created order model

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,6 +1,23 @@
 from django.contrib import admin
-from .models import Product
+from .models import *
+from django.contrib.auth.models import Group
 
-# Register your models here.
+admin.site.site_header = "VEDInventory Dashboard"
 
-admin.site.register(Product)
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'quantity', 'category',)
+    list_filter = ('category',)
+
+
+admin.site.register(Product, ProductAdmin)
+
+
+# class OrderAdmin(admin.ModelAdmin):
+#     list_display = ('product', 'staff', )
+#     list_filter = ('product',)
+
+
+admin.site.register(Order)
+
+# admin.site.unregister(Group)
--- dashboard/models.py
+++ dashboard/models.py
@@ -1,10 +1,11 @@
-from sre_parse import CATEGORIES
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
 
 CATEGORIES = (
-	('stationary', 'stationary'),
+	('Stationary', 'Stationary'),
 	('Electronics', 'Electronics'),
 	('Food', 'Food'),
 )
@@ -17,4 +18,17 @@ class Product(models.Model):
 		verbose_name_plural = 'Products'
 
 	def __str__(self):
-		return f"{self.name} - {self.quantity}"
+		return f"{self.name}"
+
+
+class Order(models.Model):
+	product = models.ForeignKey(Product, on_delete=models.CASCADE, null=True)
+	staff = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+	order_quantity = models.PositiveIntegerField(null=True)
+	date_of_order = models.DateTimeField(auto_now_add=True)
+
+	class Meta:
+		verbose_name_plural = 'Orders'
+
+	def __str__(self):
+		return f"{self.product} - ordered by {self.staff.username} - on {self.date_of_order}"
