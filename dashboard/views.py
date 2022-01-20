0e7ae9ebf2cbd36b66aa4159b0da9ad9b69e4936
Victor Dziedzorm <dicksonsmart0711@gmail.com>
2022-01-20 08:31:00 +0000

    Updated urls and installed decouple

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def index(request):
+
+	return HttpResponse("<h1>This is the index page</h1>")
+
+def staff(request):
+
+	return HttpResponse("<h1>This is the staff page</h1>")
