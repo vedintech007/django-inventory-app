d0c6447c29f25d644795f679445b4af9a4bbdf9f
Victor Dziedzorm <dicksonsmart0711@gmail.com>
2022-01-20 14:08:14 +0000

    Created products model

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,23 @@
+from sre_parse import CATEGORIES
+from statistics import quantiles
+from unicodedata import category
 from django.db import models
 
 # Create your models here.
+
+CATEGORIES = (
+	('stationary', 'stationary'),
+	('Electronics', 'Electronics'),
+	('Food', 'Food'),
+)
+class Product(models.Model):
+	name = models.CharField(max_length=100, null=True)
+	category = models.CharField(max_length=20, choices=CATEGORIES, null=True)
+	quantity = models.PositiveIntegerField(null=True)
+
+	class Meta:
+		verbose_name = 'Product'
+		verbose_name_plural = 'Products'
+
+	def __str__(self):
+		return self.name
