a46ae1663fb0c8464d5a9f382c2e65be369aa835
Victor Dziedzorm <dicksonsmart0711@gmail.com>
2022-01-26 02:31:33 +0000

    Done with order page list and detail view

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from .models import Product
+from .models import Product, Order
 from django.contrib.auth.models import User
 
 from .forms import ProductForm
@@ -100,7 +100,10 @@ def product_update(request, pk):
 
 @login_required
 def order(request):
+    orders = Order.objects.all()
 
-    context = {}
+    context = {
+        'orders': orders
+    }
 
     return render(request, 'dashboard/order.html', context)
